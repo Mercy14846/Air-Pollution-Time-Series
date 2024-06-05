4de5f01dd87275d29232b166a089c4f1802562f0
Mercy Onaopemipo Akintola <YOUR_EMAIL>
2024-06-05 16:34:00 +0100

    changed date and name

--- data collection.py
+++ data collection.py
@@ -16,8 +16,8 @@ params = {
 	"latitude": [7.9742417, 7.94112, 7.94493, 7.9640833, 7.9641133, 7.977995, 7.98917, 7.95359, 7.9817967, 7.9678433, 7.94647, 7.9821433, 7.9445333, 7.979075, 7.9743167, 7.97493, 7.9734433, 7.945825, 7.9640567, 7.98882, 7.9678517, 7.9600033, 7.9678483],
 	"longitude": [3.4971033, 3.481975, 3.5234917, 3.5088067, 3.5096683, 3.4903783, 3.5476083, 3.5562017, 3.4925283, 3.5595267, 3.54655, 3.5593967, 3.553905, 3.5579183, 3.5158967, 3.5019883, 3.526635, 3.5479517, 3.5097667, 3.5479483, 3.5595333, 3.5660183, 3.5592567],
 	"hourly": ["pm10", "pm2_5", "carbon_monoxide", "sulphur_dioxide"],
-	"start_date": "2023-11-01",
-	"end_date": "2023-11-30"
+	"start_date": "2024-03-01",
+	"end_date": "2024-03-30"
 }
 responses = openmeteo.weather_api(url, params=params)
 
@@ -49,8 +49,8 @@ hourly_data["sulphur_dioxide"] = hourly_sulphur_dioxide
 hourly_dataframe = pd.DataFrame(data = hourly_data)
 print(hourly_dataframe)
 
-hourly_dataframe.to_csv('nov_air_quality_data.csv', index=False)
-csv_file_path = 'nov_air_quality_data.csv'
+hourly_dataframe.to_csv('mar_air_quality_data.csv', index=False)
+csv_file_path = 'mar_air_quality_data.csv'
 
 # Load the CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
