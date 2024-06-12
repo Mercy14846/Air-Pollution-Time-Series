83e73fe4c20340eb7ef04a3019bc24d78ca75a10
Mercy Onaopemipo Akintola <YOUR_EMAIL>
2024-06-12 21:34:47 +0100

    Name

--- time_mat.py
+++ time_mat.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load the data from the CSV file
-csv_file = 'weekly_feb_2024.csv'
+csv_file = 'weekly_Feb_24.csv'
 data = pd.read_csv(csv_file)
 
 # Convert the start_date column to datetime
@@ -10,7 +10,7 @@ data['start_date'] = pd.to_datetime(data['start_date'])
 
 # Define a function to plot time series data for each pollutant
 def plot_time_series(data, pollutant, title, ylabel):
-    plt.figure(figsize=(14, 5))
+    plt.figure(figsize=(25, 10))
     for key, grp in data.groupby(['latitude', 'longitude']):
         plt.plot(grp['start_date'], grp[pollutant], label=f'Location: {key}')
     
@@ -22,7 +22,7 @@ def plot_time_series(data, pollutant, title, ylabel):
     plt.show()
 
 # Plot CO time series
-plot_time_series(data, 'co', 'CO Time Series', 'CO (μg/m³)')
+plot_time_series(data, 'co', 'CO Time Series for Ogbomosho', 'CO (μg/m³)')
 
 # Plot SO2 time series
 # plot_time_series(data, 'so2', 'SO2 Time Series', 'SO2 (μg/m³)')
